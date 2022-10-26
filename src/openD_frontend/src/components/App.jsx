393255298b6cda9743f62c71352372004b08466d
brandon kwamou <b.kwamoutchamou@gmail.com>
2022-10-26 13:22:55 +0200

    Step 3 Completed

--- src/openD_frontend/src/components/App.jsx
+++ src/openD_frontend/src/components/App.jsx
@@ -3,12 +3,15 @@ import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeImage from "../../assets/home-img.png";
+import Item from "./Item";
 
 function App() {
+	const id = "rrkah-fqaaa-aaaaa-aaaaq-cai";
 	return (
 		<div className="App">
 			<Header />
-			<img className="bottom-space" src={homeImage} />
+			<Item id={id} />
+			{/* <img className="bottom-space" src={homeImage} /> */}
 			<Footer />
 		</div>
 	);
